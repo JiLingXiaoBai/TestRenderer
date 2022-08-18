c94c71e4f1dac136909e4b1fc5c349ccf634ae6e
JiLingXiaoBai <uiojkl3588@qq.com>
2022-08-18 18:36:37 +0800

    画线算法

---
+++ Canvas.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRenderer
+{
+    internal class Canvas
+    {
+        private void TempSwap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        //Bresenham算法
+        public void DrawLine(int x0, int y0, int x1, int y1, Bitmap bitmap, Color color)
+        {
+            bool steep = false;
+            if(Math.Abs(x0 - x1) < Math.Abs(y0 - y1))
+            {
+                TempSwap(ref x0, ref y0);
+                TempSwap(ref x1, ref y1);
+                steep = true;
+            }
+
+            if(x0 > x1)
+            {
+                TempSwap(ref x0, ref x1);
+                TempSwap(ref y0, ref y1);
+            }
+
+            int dx = x1 - x0;
+            int dy = y1 - y0;
+
+            int derror = Math.Abs(dy) * 2;
+            int error = 0;
+            int y = y0;
+
+            for(int x = x0; x <= x1; x++)
+            {
+                if (steep)
+                {
+                    bitmap.SetPixel(y, x, color);
+                }
+                else
+                {
+                    bitmap.SetPixel(x, y, color);
+                }
+
+                error += derror;
+
+                if (error > dx)
+                {
+                    y += (y1 > y0 ? 1 : -1);
+                    error -= dx * 2;
+                }
+            }
+        }
+    }
+}
--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(45, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
     }
 }
